3f54149f2143cc2dd4bbe5bcbeec3ec1d8c8fa3e
AWM <awilbu2@uic.edu>
2025-01-24 22:40:06 -0600

    can pull all observations at once now thanks to Open FEMA update

--- scripts/pull_NFIP_redactedclaims.R
+++ scripts/pull_NFIP_redactedclaims.R
@@ -106,18 +106,15 @@ result <- GET(record_check_query)
 jsonData <- httr::content(result)        
 n_records <- jsonData$metadata$count 
 
-# calculate number of calls neccesary to get all records using the 
+# calculate number of calls necessary to get all records using the 
 # 1000 records/ call max limit defined by FEMA
 iterations <- ceiling(n_records / 1000)
 
 # initialize a skip counter which will indicate where in the full 
 # data set each API call needs to start from.
-skip <- 0
-
-stop100 <- 100
 
 # make however many API calls are neccesary to get the full data set
-for(i in seq(from = 1, to = stop100, by = 1)){
+for(i in seq(from = 1, to = iterations, by = 1)){
   # As above, if you have filters, specific fields, or are sorting, add
   # that to the base URL or make sure it gets concatenated here.
   result <- httr::GET(paste0(api_query, "&$skip=", (i - 1) * 1000))
@@ -151,62 +148,60 @@ for(i in seq(from = 1, to = stop100, by = 1)){
 }
 
 data <- as.data.frame(lapply(data, function(data) gsub("\n", "", data)))
+data_first100K <- data
 
+# data %>% write_csv("./inputs/data/nfippolicies_CookCounty2.csv")
 
-
-
-
-# Can only pull 100,000 at a time, I think? 
-# now do the remaining observations
-
-base_url <- "https://www.fema.gov/api/open/v2/FimaNfipPolicies"
-
-# append the base_url to apply filters
-#filters <- "?$inlinecount=allpages&$filter=(state%20eq%20'IL')"
-filters <- "?$inlinecount=allpages&$filter=(countyCode%20eq%20'17031'&$skip=100000)"
+# now do the remaining observations for policies ########
+filters <- "?$inlinecount=allpages&$filter=(countyCode%20eq%20'17031')&$allrecords=true"
 
 api_query <- paste0(base_url, filters)
+result <- httr::GET(paste0(api_query))
+jsonData <- httr::content(result)[[2]]
+data <- data.frame(do.call(rbind, jsonData))
+data <- as.data.frame(lapply(data, function(data) gsub("\n", "", data)))
+data %>% write_csv("./inputs/data/nfippolicies_CookCounty_all.csv")
 
-start <- 101
-for(i in seq(from = start, to = iterations, by = 1)){
-  # As above, if you have filters, specific fields, or are sorting, add
-  # that to the base URL or make sure it gets concatenated here.
-  result <- httr::GET(paste0(api_query,(i - 1) * 1000))
-  if (result$status_code != 200) {
-    status <- httr::http_status(result)
-    stop(status$message)
-  }
-  jsonData <- httr::content(result)[[2]]
-  
-  # for data returned as a list of lists, correct any discrepancies
-  # in the length of the lists by adding NA values to the shorter lists
-  
-  # calculate longest list
-  max_list_length <- max(sapply(jsonData, length))
-  
-  # add NA values to lists shorter than the max list length
-  jsonData <- lapply(jsonData, function(x) {
-    c(x, rep(NA, max_list_length - length(x)))
-    
-  })
-  
-  if (i == 101) {
-    # bind the data into a single data frame
-    data2 <- data.frame(do.call(rbind, jsonData))
-  } else {
-    data2 <- dplyr::bind_rows(
-      data2,
-      data.frame(do.call(rbind, jsonData))
-    )
-  }
-}
-
-
-# remove the html line breaks from returned data frame (if there are any)  
-data2 <- as.data.frame(lapply(data2, function(data2) gsub("\n", "", data2)))
+# 
+# for(i in seq(from = 1, to = 2, by = 1)){
+#   # As above, if you have filters, specific fields, or are sorting, add
+#   # that to the base URL or make sure it gets concatenated here.
+#   result <- httr::GET(paste0(api_query))
+#   if (result$status_code != 200) {
+#     status <- httr::http_status(result)
+#     stop(status$message)
+#   }
+#   jsonData <- httr::content(result)[[2]]
+#   
+#   # for data returned as a list of lists, correct any discrepancies
+#   # in the length of the lists by adding NA values to the shorter lists
+#   
+#   # calculate longest list
+#   max_list_length <- max(sapply(jsonData, length))
+#   
+#   # add NA values to lists shorter than the max list length
+#   jsonData <- lapply(jsonData, function(x) {
+#     c(x, rep(NA, max_list_length - length(x)))
+#     
+#   })
+#   
+#   # if (i == 1) {
+#     # bind the data into a single data frame
+#     data <- data.frame(do.call(rbind, jsonData))
+#   # } else {
+#   #   data <- dplyr::bind_rows(
+#   #     data,
+#   #     data.frame(do.call(rbind, jsonData))
+#   #   )
+#   # }
+# }
+# 
+# 
+# # remove the html line breaks from returned data frame (if there are any)  
+# data <- as.data.frame(lapply(data, function(data) gsub("\n", "", data)))
 
 # view the retrieved data
 # data
 
-data %>% write_csv("./inputs/data/nfippolicies_CookCounty.csv")
+# data %>% write_csv("./inputs/data/nfippolicies_CookCounty_all.csv")
 
