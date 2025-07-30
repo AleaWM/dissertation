7b4c8f3786f60f00875a3f7459c2c931a193d7f4
AWM <awilbu2@uic.edu>
2025-07-29 22:25:44 -0500

    saves individual assistance file with cleaned up municipality names. For question 3.

--- scripts/clean_damagedCity_names.R
+++ scripts/clean_damagedCity_names.R
@@ -13,7 +13,7 @@ ind_assist <- read_csv("./data/raw/indiv_assistance_V2_CookCounty.csv") |>
            ifelse(year < 2020 & year > 2010, 2010,
                   ifelse(year > 2020, 2020, year))),
          tract = str_sub(censusGeoid, 6, 9)) |>
-  mutate(tract = str_pad(tract, width = 6, pad = "0", side = "right")) |>
+  mutate(tract = str_pad(tract, width = 6, pad = "0", side = "right"))
 
 # all disasters in Cook County, IL
 cook_assist <- ind_assist %>% filter(county == "Cook (County)")
@@ -35,5 +35,13 @@ cities <- cook_assist |> distinct(damagedCity)
 # export CSV of messy names to make chatGPT identify ones that should be recoded
 # cities |> write_csv("./data/raw/ia_city_names.csv") 
 
+names <- read_csv("./data/processed/City_Name_Mapping.csv")
 
+cook_assist <- cook_assist |> left_join(names)
 
+table(cook_assist$damagedCity[ind_assist$disasterNumber=="4728"])
+
+table(cook_assist$clean_name[ind_assist$disasterNumber=="4728"])
+
+write_csv("./data/processed/indiv_assistance_V2_CookCounty_wNames.csv")
+  
