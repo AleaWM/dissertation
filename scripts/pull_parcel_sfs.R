1c3ffa0eccf946b33139f0edc57fc8dbcad3f6a9
AWM <awilbu2@uic.edu>
2026-01-12 10:50:00 -0600

    comments out unused code at bottom.
    Uses 2026 Pending S_FIRM_PAN.shp.
    
    Creates additional output that has merged SFHA indicators to parcels with FIRM info.

--- scripts/pull_parcel_sfs.R
+++ scripts/pull_parcel_sfs.R
@@ -3,7 +3,7 @@
 # identifies which FIRM they are in! (using the 2024 State NFHL - WARNING some FIRMS changed shapes!)
 #
 # Input(s): ptaxsim database
-# Output(s):  
+# Output(s):
 #             "data/processed/parcels_wFIRMs.csv"
 #             "./data/raw/parcel_shapefiles_ty2023.gpkg" <---  Not used
 #             "./data/raw/parcel_shapefiles_ty2018.gpkg" <--- not used
@@ -23,10 +23,10 @@ library(sf)
 library(beepr)
 library(tictoc)
 
-#PTAXSIM has parcel centroids
+# PTAXSIM has parcel centroids
 ptaxsim_db_conn <- DBI::dbConnect(RSQLite::SQLite(), "../Merriman RA/ptax/ptaxsim.db/ptaxsim-2023.0.0.db")
 
-border <- st_read("inputs/Mapping_FIRMs/Cook_County_Border/Cook_County_Border.shp") |> 
+border <- st_read("inputs/Mapping_FIRMs/Cook_County_Border/Cook_County_Border.shp") |>
   st_transform("EPSG:4326")
 
 common_crs <- st_crs(border)
@@ -41,7 +41,7 @@ pin_geoms <- DBI::dbGetQuery(
   FROM pin_geometry_raw
   ",
     .con = ptaxsim_db_conn
-  ))
+))
 
 class(pin_geoms)
 # Convert character WKT geometry to sf object
@@ -74,30 +74,34 @@ pin_centroids <- DBI::dbGetQuery(
   FROM pin_geometry_raw
   ",
     .con = ptaxsim_db_conn
-  ))
+))
 
 # Manually convert lat/lon to point
+# some parcels had start and stop years split up across observations
 pin_centroids <- pin_centroids |>
   group_by(pin10) |>
   summarize(longitude = first(longitude),
-            latitude = first(latitude),
-            start_year = min(start_year),
-            end_year = max(end_year)) |>
-  st_as_sf(coords = c("longitude", "latitude"), crs = common_crs, remove=FALSE)
+    latitude = first(latitude),
+    start_year = min(start_year),
+    end_year = max(end_year)) |>
+  st_as_sf(coords = c("longitude", "latitude"), crs = common_crs, remove = FALSE)
 
 
-#pin_centroids <- sf_data |> mutate(centroid = st_centroid(geometry))
+# pin_centroids <- sf_data |> mutate(centroid = st_centroid(geometry))
 
 
 ## reads in FIRM layer from statewide geodatabase.
 ## clips it to just Cook County with `border` shapefile
-st_clip_firms_2024 <- read_sf("inputs/Mapping_Firms/NFHL_17_20240628/Statewide_NFHL_17_20240628.gdb",
-                              layer = "S_FIRM_PAN") |>  
+# Pretend it is named ...firms_2026 for now.
+st_clip_firms_2024 <- st_read("inputs/Cook_2026_download/S_FIRM_Pan.shp")
+# st_clip_firms_2024 <- read_sf("inputs/Mapping_Firms/NFHL_17_20240628/Statewide_NFHL_17_20240628.gdb",
+#                               layer = "S_FIRM_PAN") |>
+st_clip_firms_2024 <- st_clip_firms_2024 |>
   st_transform(common_crs) |>
-  st_intersection(border) |>  
- # st_within(border) |>
+  st_intersection(border) |>
+  #  st_within(border) |>
   st_transform(common_crs) #|>
-# 
+#
 #   mutate(
 #     pre_date = paste0(year(PRE_DATE), "-",
 #                       str_pad(month(PRE_DATE), width = 2, side = "left", pad = "0"), "-",
@@ -107,7 +111,7 @@ st_clip_firms_2024 <- read_sf("inputs/Mapping_Firms/NFHL_17_20240628/Statewide_N
 #                       str_pad(day(PRE_DATE), width=2, side = "left", pad = "0"))
 #   )
 
-st_bbox(st_clip_firms_2024) 
+st_bbox(st_clip_firms_2024)
 
 
 st_clip_firms_2024 <- st_make_valid(st_clip_firms_2024)
@@ -117,12 +121,18 @@ st_geometry(st_clip_firms_2024) <- "SHAPE"
 
 # EFFECTIVE FIRMS as of 2024 from State NFHL
 ggplot() +
-  geom_sf(data = st_clip_firms_2024 |> filter(DFIRM_ID == "17031C"), 
-          linewidth = 0.3, aes(fill = factor(EFF_DATE) ) ) +
-  geom_sf(data = border, fill = NA, color = "black", lwd=1)+
+  geom_sf(data = st_clip_firms_2024 |> filter(DFIRM_ID == "17031C"),
+    linewidth = 0.3, aes(fill = factor(EFF_DATE))) +
+  geom_sf(data = border, fill = NA, color = "black", lwd = 1) +
   theme_void() +
- #scale_fill_date() +
-  labs( title = "FIRM Effective Date" , fill = "" )
+  # scale_fill_date() +
+  labs(title = "FIRM Effective Date", fill = "") +
+  # scale_fill_manual(values = c(
+  #   "#E69F00", "#56B4E9", "#009E73", "#F0E442",
+  #   "#0072B2", "#D55E00", "#CC79A7", "#999999"
+  # ))
+  scale_fill_grey(start = 0.85, end = 0.2)
+
 
 
 # 1,457,163  distinct pin10 obs
@@ -131,95 +141,134 @@ table(pin_centroids$DFIRM_ID) # check to see if it worked
 
 # are there duplicate rows for parcels? -- Yes
 # there are also parcels that have start year 2006-2012 and then 2013-2023
-n_distinct(pin_centroids$pin10) 
-#1,457,163 distinct. Many duplicates. Mostly due to weird start and stop years. 
-dups <- pin_centroids |> 
-  group_by(pin10) |> 
+n_distinct(pin_centroids$pin10)
+# 1,457,163 distinct. Many duplicates. Mostly due to weird start and stop years.
+dups <- pin_centroids |>
+  group_by(pin10) |>
   mutate(dup_count = n(),
-         in_cook = ifelse(DFIRM_ID == "17031C", 1, 0)) |> 
+    in_cook = ifelse(DFIRM_ID == "17031C", 1, 0)) |>
   filter(dup_count > 1)
 
-dups_cook <- dups |> 
-  filter( in_cook == 1)
-dups_notcook <- dups |> 
-  filter( in_cook == 0)
+dups_cook <- dups |>
+  filter(in_cook == 1)
+dups_notcook <- dups |>
+  filter(in_cook == 0)
 
 pin_centroids_clean <- anti_join(as.data.frame(pin_centroids), as.data.frame(dups_notcook))
 
-pin_centroids_clean <- pin_centroids_clean |> as.data.frame() |> select(-c(PCOMM, PANEL, SUFFIX, ST_FIPS, GFID:SHAPE_Area.1))
+pin_centroids_clean <- pin_centroids_clean |> as.data.frame() |> select(-c(PCOMM, PANEL, SUFFIX, ST_FIPS))
 
-pin_centroids_clean <- pin_centroids_clean |> select(-c(SCALE, PANEL_TYP, PNP_REASON, BASE_TYP,  geometry) )
+pin_centroids_clean <- pin_centroids_clean |> select(-c(SCALE, PANEL_TYP, PNP_REASON, BASE_TYP,  geometry))
 
-write_csv(pin_centroids_clean, "data/processed/parcels_wFIRMs_20250604.csv")
-
-
+write_csv(pin_centroids_clean, "data/processed/parcels_wFIRMs_2026.csv")
 
 
 
+parcels_wFIRMs <- read_csv("data/processed/parcels_wFIRMs_2026.csv") |>
+  select(-c(PRE_DATE, EFF_DATE, OBJECTID_1, VERSION_ID, FIRM_ID, DFIRM_ID))
 
-####### The rest is not currently used ##############
-tic()
-beep_on_error(
-  sf_data <- st_join(sf_data, st_clip_firms_2024, join = st_within), sound = "wilhelm"  )
-write_sf(sf_data, "./data/processed/parcel_polygons_wFIRMs.gpkg" )
- beep("coin")
-toc()
+library(readxl)
+firm_info <- read_xlsx("inputs/Cook_2026_download/S_FIRM_PAN.xlsx") |>
+  select(FIRM_PAN, VERSION_ID, FIRM_ID, PRE_DATE, EFF_DATE)
+# select(-c(DFIRM_ID, FID, PCOMM, ST_FIPS, PANEL_TYP, SCALE, ))
 
-# st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.shp", layer = "parcels", driver = "ESRI Shapefile") 
-st_write(sf_data, "./data/raw/parcel_shapefiles.gpkg", layer = "parcels") 
+parcels_wFIRMs <- left_join(parcels_wFIRMs, firm_info, by = "FIRM_PAN")
 
+table(parcels_wFIRMs$PRE_DATE)
+table(parcels_wFIRMs$EFF_DATE)
 
+pin_indicators <-  read_csv("./data/processed/sfha_indicator_parcels_20260107_V2.csv") |>
+  mutate(pin10 = str_pad(pin10, 10, "left", 0))
 
+pin_indicators <- full_join(parcels_wFIRMs, pin_indicators, by = "pin10")
 
-## PINS and then parcels, not as efficient -------------------------------------
+pin_indicators <- pin_indicators |>
+  mutate(in_prelim_panels = ifelse(VERSION_ID == "2.6.3.6", TRUE, FALSE))
 
-all_pins <- DBI::dbGetQuery(
-  ptaxsim_db_conn,
-  glue_sql(
-    "SELECT DISTINCT pin 
-  FROM pin
-  ",
-    .con = ptaxsim_db_conn
-  ))
+pin_indicators <- pin_indicators |>
+  mutate(
+    sfha2018 = ifelse(!is.na(FLD_ZONE), 1, 0),
+    sfha2024 = ifelse(!is.na(FLD_ZONE2024), 1, 0),
+    lomr2018 =  ifelse(!is.na(LOMR_IDlomr2018), 1, 0),
+    lomr2024 = ifelse(!is.na(LOMR_IDlomr2024), 1, 0)
+  ) |>
+  mutate(sfha2026 =
+    ifelse(in_prelim_panels == TRUE & !is.na(FLD_ZONEprelim), 1,
+      ifelse(in_prelim_panels == TRUE & is.na(FLD_ZONEprelim), 0,
+        ifelse(in_prelim_panels == FALSE, sfha2024, "CHECKME")))
+  )
+table(pin_indicators$sfha2018)
 
-all_parcels <- all_pins |> 
-  mutate(pin = str_pad(pin, 14, side = "left", pad = "0"),
-         pin10 = str_sub(pin, 1, 10)) |>
-  distinct(pin10) 
+table(pin_indicators$sfha2024)
 
-## 2018 shapefiles ---------------------------------------------------
+table(pin_indicators$sfha2026)
 
-
-
-
-years <- c(2018)
-fh_pins_geo <- lookup_pin10_geometry(year = years, pin10 = all_parcels$pin10)
-library(sf)
-
-# Convert character WKT geometry to sf object
-sf_data <- fh_pins_geo |>
-  st_as_sf(wkt = "geometry", crs = 6465)
-
-# Set the correct CRS if missing (update EPSG code as needed)
-st_crs(sf_data) <- 6454  # Projection for Illinois. Makes skinny Cook.
-sf_data <- st_transform(sf_data, crs = 6454)
-sf_data <-st_cast(sf_data, "MULTIPOLYGON")
-# st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.shp", layer = "parcels", driver = "ESRI Shapefile") 
-st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.gpkg", layer = "parcels") 
+write_csv(pin_indicators, "./data/processed/parcels_withFIRMs_20260108.csv")
 
 
-## 2023 Shapefiles -------------------------------------------------------------
-years <- c(2023)
-fh_pins_geo <- lookup_pin10_geometry(year = years, pin10 = all_parcels$pin10)
-library(sf)
-
-# Convert character WKT geometry to sf object
-sf_data <- fh_pins_geo |>
-  st_as_sf(wkt = "geometry", crs = 6454)
-
-# Set the correct CRS if missing (update EPSG code as needed)
-st_crs(sf_data) <- 6454  # Projection for Illinois. Makes skinny Cook.
-sf_data <- st_transform(sf_data, crs = 6454)
-
-# st_write(sf_data, "./data/raw/parcel_shapefiles_ty2023.shp", layer = "parcels", driver = "ESRI Shapefile") 
-st_write(sf_data, "./data/raw/parcel_shapefiles_ty2023.gpkg", layer = "parcels") 
+# ####### The rest is not currently used ##############
+# tic()
+# beep_on_error(
+#   sf_data <- st_join(sf_data, st_clip_firms_2024, join = st_within), sound = "wilhelm")
+# write_sf(sf_data, "./data/processed/parcel_polygons_wFIRMs.gpkg")
+# beep("coin")
+# toc()
+#
+# # st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.shp", layer = "parcels", driver = "ESRI Shapefile")
+# st_write(sf_data, "./data/raw/parcel_shapefiles.gpkg", layer = "parcels")
+#
+#
+#
+#
+# ## PINS and then parcels, not as efficient -------------------------------------
+#
+# all_pins <- DBI::dbGetQuery(
+#   ptaxsim_db_conn,
+#   glue_sql(
+#     "SELECT DISTINCT pin
+#   FROM pin
+#   ",
+#     .con = ptaxsim_db_conn
+# ))
+#
+# all_parcels <- all_pins |>
+#   mutate(pin = str_pad(pin, 14, side = "left", pad = "0"),
+#     pin10 = str_sub(pin, 1, 10)) |>
+#   distinct(pin10)
+#
+# ## 2018 shapefiles ---------------------------------------------------
+#
+#
+#
+#
+# years <- c(2018)
+# fh_pins_geo <- lookup_pin10_geometry(year = years, pin10 = all_parcels$pin10)
+# library(sf)
+#
+# # Convert character WKT geometry to sf object
+# sf_data <- fh_pins_geo |>
+#   st_as_sf(wkt = "geometry", crs = 6465)
+#
+# # Set the correct CRS if missing (update EPSG code as needed)
+# st_crs(sf_data) <- 6454  # Projection for Illinois. Makes skinny Cook.
+# sf_data <- st_transform(sf_data, crs = 6454)
+# sf_data <- st_cast(sf_data, "MULTIPOLYGON")
+# # st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.shp", layer = "parcels", driver = "ESRI Shapefile")
+# st_write(sf_data, "./data/raw/parcel_shapefiles_ty2018.gpkg", layer = "parcels")
+#
+#
+# ## 2023 Shapefiles -------------------------------------------------------------
+# years <- c(2023)
+# fh_pins_geo <- lookup_pin10_geometry(year = years, pin10 = all_parcels$pin10)
+# library(sf)
+#
+# # Convert character WKT geometry to sf object
+# sf_data <- fh_pins_geo |>
+#   st_as_sf(wkt = "geometry", crs = 6454)
+#
+# # Set the correct CRS if missing (update EPSG code as needed)
+# st_crs(sf_data) <- 6454  # Projection for Illinois. Makes skinny Cook.
+# sf_data <- st_transform(sf_data, crs = 6454)
+#
+# # st_write(sf_data, "./data/raw/parcel_shapefiles_ty2023.shp", layer = "parcels", driver = "ESRI Shapefile")
+# st_write(sf_data, "./data/raw/parcel_shapefiles_ty2023.gpkg", layer = "parcels")
