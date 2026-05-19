78deb56b4dc935934fb6707860931b3293d6274f
AWM <awilbu2@uic.edu>
2026-05-19 11:49:35 -0500

    changed data source for parcel shapefiles
    changed references to sfha_2024 to sfha_2026 and removed references to preliminary FIRM data once it was no longer needed (after January 2026)

--- R/sfha_targets_functions.R
+++ R/sfha_targets_functions.R
@@ -9,23 +9,48 @@ read_border <- function(border_shp, crs_out = 6454) {
     st_transform(crs_out)
 }
 
+# for recently downloaded file from Cook Parcel Viewer
 read_parcels_2018 <- function(gdb_path, border, crs_out) {
-  st_read(gdb_path, layer = "parcel_2018_parcel", quiet = TRUE) |>
-    st_transform(crs_out) |>
+  st_read(gdb_path, layer = "parcels_2018", quiet = TRUE) |>
+    sf::st_transform(crs_out) |>
+    (\(x) x[st_is_valid(x), ])()
+
+}
+
+# # for archived 2018 parcels that were downloaded from Open Data site
+# read_parcels_2018 <- function(gdb_path, border, crs_out) {
+#   st_read(gdb_path, layer = "parcel_2018_parcel", quiet = TRUE) |>
+#     sf::st_transform(crs_out) |>
+#     (\(x) x[st_is_valid(x), ])()
+#
+# }
+
+read_parcels_2022 <- function(gdb_path, border, crs_out) {
+  sf::st_read(gdb_path, layer = "parcel_2022_parcel2022_enhanced", quiet = TRUE) |>
+    sf::st_transform(crs_out) |>
     (\(x) x[st_is_valid(x), ])()
 }
 
-read_parcels_2022 <- function(gdb_path, border, crs_out) {
-  st_read(gdb_path, layer = "parcel_2022_parcel2022_enhanced", quiet = TRUE) |>
-    st_transform(crs_out) |>
+
+read_parcels_2025 <- function(gdb_path, border, crs_out) {
+  sf::st_read(gdb_path, layer = "parcels_2025", quiet = TRUE) |>
+    sf::st_transform(crs_out) |>
     (\(x) x[st_is_valid(x), ])()
 }
+
+# read_parcels_2025 <- function(gdb_path, border, crs_out) {
+#   sf::st_read("inputs/cook_parcels_2025.gpkg", layer = "parcels_2025", quiet = TRUE) |>
+#     sf::st_transform(crs_out)
+#
+#
+# }
 
 read_nfhl_sfha <- function(gdb_path, layer, border, sfha_tf_field = "SFHA_TF") {
   sf::read_sf(gdb_path, layer = layer, quiet = TRUE) |>
     filter(.data[[sfha_tf_field]] == "T") |>
     st_transform(st_crs(border)) |>
-    st_intersection(border) |>
+    st_intersection(border)  |>
+    st_make_valid() |>
     st_cast("MULTIPOLYGON")
 }
 
@@ -33,6 +58,7 @@ read_nfhl_lomr <- function(gdb_path, layer, border) {
   sf::read_sf(gdb_path, layer = layer, quiet = TRUE) |>
     st_transform(st_crs(border)) |>
     st_intersection(border) |>
+    st_make_valid() |>
     st_cast("MULTIPOLYGON")
 }
 
@@ -43,6 +69,7 @@ read_prelim_sfha <- function(prelim_shp, border, crs_out = sf::st_crs(border)) {
     filter(SFHA_TF == "T") |>
     st_transform(st_crs(border)) |>
     st_intersection(border) |>
+    st_make_valid() |>
     st_cast("MULTIPOLYGON")
 }
 
@@ -60,6 +87,14 @@ read_firm_panels_shp <- function(shp_path, border, crs_out) {
 }
 
 
+# # identifies parcels in SFHA == T Zones
+# join_parcels_to_zone <- function(parcels, zone_poly, id_field) {
+#   # NOTE: st_join is expensive; keep it in one place.
+#   sf_use_s2(TRUE)
+#   out <- st_join(parcels, zone_poly, join = st_intersects)
+#   out |> filter(!is.na(.data[[id_field]]))
+# }
+
 # identifies parcels in SFHA == T Zones
 join_parcels_to_zone <- function(parcels, zone_poly, id_field) {
   # NOTE: st_join is expensive; keep it in one place.
@@ -68,7 +103,85 @@ join_parcels_to_zone <- function(parcels, zone_poly, id_field) {
   out |> filter(!is.na(.data[[id_field]]))
 }
 
-make_sfha_indicator_parcels <- function(sfha2018, sfha2024, prelim, lomr2018, lomr2024) {
+#
+# make_sfha_indicator_parcels <- function(
+#    sfha2018, sfha2024, prelim, lomr2018, lomr2024   # original targets line of code
+# ) {
+#   # Helper: only filter on end_year if it exists (PTAXSIM inputs)
+#   maybe_filter_end_year <- function(x, cutoff) {
+#     x_df <- x |> as.data.frame()
+#     if ("end_year" %in% names(x_df)) {
+#       x_df |> dplyr::filter(.data$end_year >= cutoff)
+#     } else {
+#       x_df
+#     }
+#   }
+#
+#
+#   # ---- SFHA rollups ----
+#   sfha2018_df <- sfha2018 |>
+#     maybe_filter_end_year(2018) |>
+#     dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
+#     dplyr::select(.data$pin10, .data$FLD_ZONE) |>
+#     dplyr::group_by(.data$pin10) |>
+#     dplyr::slice_head(n = 1) |>
+#     dplyr::ungroup()
+#
+#   sfha2024_df <- sfha2024 |>
+#     maybe_filter_end_year(2022) |>
+#     dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
+#     dplyr::select(.data$pin10, .data$FLD_ZONE) |>
+#     dplyr::group_by(.data$pin10) |>
+#     dplyr::slice_head(n = 1) |>
+#     dplyr::ungroup()
+#
+#   prelim_df <- prelim |>
+#     maybe_filter_end_year(2021) |>
+#     dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
+#     dplyr::select(.data$pin10, .data$FLD_ZONE) |>
+#     dplyr::group_by(.data$pin10) |>
+#     dplyr::slice_head(n = 1) |>
+#     dplyr::ungroup() |>
+#     dplyr::mutate(in_prelim = "PrePanel")
+#
+#   # ---- LOMR rollups ----
+#   # (Optional: you *can* also filter LOMRs by end_year if the inputs have it,
+#   # but most of the time LOMR layers won’t.)
+#   lomr2018_df <- lomr2018 |>
+#     as.data.frame() |>
+#     dplyr::mutate(lomr_year = "2018") |>
+#     dplyr::select(.data$pin10, .data$lomr_year, .data$EFF_DATE, .data$CASE_NO) |>
+#     dplyr::distinct()
+#
+#   lomr2024_df <- lomr2024 |>
+#     as.data.frame() |>
+#     dplyr::mutate(lomr_year = "2024") |>
+#     dplyr::select(.data$pin10, .data$lomr_year, .data$EFF_DATE, .data$CASE_NO) |>
+#     dplyr::distinct()
+#
+#   lomr_join <- lomr2018_df |>
+#     dplyr::full_join(
+#       lomr2024_df,
+#       by = c("pin10", "CASE_NO", "EFF_DATE"),
+#       suffix = c("lomr2018", "lomr2024")
+#     ) |>
+#     dplyr::group_by(.data$pin10) |>
+#     dplyr::arrange(.data$pin10, .data$EFF_DATE) |>
+#     dplyr::slice_head(n = 1) |>
+#     dplyr::ungroup() |>
+#     dplyr::rename(lomr_date = .data$EFF_DATE)
+#
+#   # ---- Combine ----
+#   out <- sfha2018_df |>
+#     dplyr::full_join(sfha2024_df, by = "pin10", suffix = c("_2018", "_2024")) |>
+#     dplyr::full_join(prelim_df, by = "pin10") |>
+#     dplyr::full_join(lomr_join, by = "pin10")
+#
+#   out
+# }
+
+# arcgis files version
+make_sfha_indicator_parcels <- function(sfha2018, sfha2026, lomr2018, lomr2026) {     # for arcgis version of files
   # Helper: only filter on end_year if it exists (PTAXSIM inputs)
   maybe_filter_end_year <- function(x, cutoff) {
     x_df <- x |> as.data.frame()
@@ -83,49 +196,46 @@ make_sfha_indicator_parcels <- function(sfha2018, sfha2024, prelim, lomr2018, lo
   # ---- SFHA rollups ----
   sfha2018_df <- sfha2018 |>
     maybe_filter_end_year(2018) |>
+    rename_with(tolower, any_of("PIN10")) |>
+
     dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
     dplyr::select(.data$pin10, .data$FLD_ZONE) |>
     dplyr::group_by(.data$pin10) |>
     dplyr::slice_head(n = 1) |>
     dplyr::ungroup()
 
-  sfha2024_df <- sfha2024 |>
-    maybe_filter_end_year(2022) |>
+  sfha2026_df <- sfha2026 |>
+    rename_with(tolower, any_of("PIN10")) |>
+    #  maybe_filter_end_year(2022) |>
     dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
     dplyr::select(.data$pin10, .data$FLD_ZONE) |>
     dplyr::group_by(.data$pin10) |>
     dplyr::slice_head(n = 1) |>
     dplyr::ungroup()
 
-  prelim_df <- prelim |>
-    maybe_filter_end_year(2021) |>
-    dplyr::arrange(.data$pin10, .data$FLD_ZONE, .data$ZONE_SUBTY) |>
-    dplyr::select(.data$pin10, .data$FLD_ZONE) |>
-    dplyr::group_by(.data$pin10) |>
-    dplyr::slice_head(n = 1) |>
-    dplyr::ungroup() |>
-    dplyr::mutate(in_prelim = "PrePanel")
 
   # ---- LOMR rollups ----
   # (Optional: you *can* also filter LOMRs by end_year if the inputs have it,
   # but most of the time LOMR layers won’t.)
   lomr2018_df <- lomr2018 |>
     as.data.frame() |>
+    rename_with(tolower, any_of("PIN10")) |>
     dplyr::mutate(lomr_year = "2018") |>
     dplyr::select(.data$pin10, .data$lomr_year, .data$EFF_DATE, .data$CASE_NO) |>
     dplyr::distinct()
 
-  lomr2024_df <- lomr2024 |>
+  lomr2026_df <- lomr2026 |>
     as.data.frame() |>
-    dplyr::mutate(lomr_year = "2024") |>
+    rename_with(tolower, any_of("PIN10")) |>
+    dplyr::mutate(lomr_year = "2026") |>
     dplyr::select(.data$pin10, .data$lomr_year, .data$EFF_DATE, .data$CASE_NO) |>
     dplyr::distinct()
 
   lomr_join <- lomr2018_df |>
     dplyr::full_join(
-      lomr2024_df,
+      lomr2026_df,
       by = c("pin10", "CASE_NO", "EFF_DATE"),
-      suffix = c("lomr2018", "lomr2024")
+      suffix = c("lomr2018", "lomr2026")
     ) |>
     dplyr::group_by(.data$pin10) |>
     dplyr::arrange(.data$pin10, .data$EFF_DATE) |>
@@ -135,8 +245,7 @@ make_sfha_indicator_parcels <- function(sfha2018, sfha2024, prelim, lomr2018, lo
 
   # ---- Combine ----
   out <- sfha2018_df |>
-    dplyr::full_join(sfha2024_df, by = "pin10", suffix = c("_2018", "_2024")) |>
-    dplyr::full_join(prelim_df, by = "pin10") |>
+    dplyr::full_join(sfha2026_df, by = "pin10", suffix = c("_2018", "_2026")) |>
     dplyr::full_join(lomr_join, by = "pin10")
 
   out
@@ -158,10 +267,26 @@ read_buildings <- function(buildings_files, border, crs_out) {
   x
 }
 
+# assign_buildings_to_parcels <- function(buildings, parcels, parcel_pin_field = "pin10") {
+#   # Keep only what we need from parcels
+#   p <- parcels |>
+#     dplyr::select(pin10, shape, municipality, politicaltownship, assessorbldgclass) |> distinct()
+#
+#   # Join: each building gets the parcel pin it falls in
+#   # left = FALSE drops buildings that fail to match a parcel
+#   b <- sf::st_join(buildings, p, join = sf::st_intersects, left = FALSE)
+#
+#   b |>
+#     dplyr::filter(!is.na(.data$pin10))
+# }
+
+
 assign_buildings_to_parcels <- function(buildings, parcels, parcel_pin_field = "pin10") {
   # Keep only what we need from parcels
   p <- parcels |>
-    dplyr::select(pin10, shape, municipality, politicaltownship, assessorbldgclass) |> distinct()
+    rename(pin10 = PIN10) |>
+    dplyr::select(pin10, geom # , municipality, politicaltownship, assessorbldgclass
+    ) |> distinct()
 
   # Join: each building gets the parcel pin it falls in
   # left = FALSE drops buildings that fail to match a parcel
@@ -171,13 +296,61 @@ assign_buildings_to_parcels <- function(buildings, parcels, parcel_pin_field = "
     dplyr::filter(!is.na(.data$pin10))
 }
 
+
 join_buildings_to_zone <- function(buildings_with_pin, zone_poly, id_field) {
   sf::sf_use_s2(TRUE)
   out <- sf::st_join(buildings_with_pin, zone_poly, join = sf::st_intersects)
   out |> dplyr::filter(!is.na(.data[[id_field]]))
 }
 
-make_sfha_indicator_buildings <- function(b2018, b2024, bprelim, blomr2018, blomr2024) {
+# make_sfha_indicator_buildings <- function(b2018, b2024, bprelim, blomr2018, blomr2024) {
+#   # --- SFHA zone rollups (pin10-level) ---
+#   roll_zone <- function(x, out_name) {
+#     x |>
+#       sf::st_drop_geometry() |>
+#       dplyr::select(pin10, FLD_ZONE) |>
+#       dplyr::filter(!is.na(.data$pin10), !is.na(.data$FLD_ZONE)) |>
+#       dplyr::group_by(.data$pin10) |>
+#       dplyr::summarise(
+#         "{out_name}" := paste(sort(unique(.data$FLD_ZONE)), collapse = ";"),
+#         .groups = "drop"
+#       )
+#   }
+#
+#   b2018_df  <- roll_zone(b2018,  "bldg_FLD_ZONE_2018")
+#   b2024_df  <- roll_zone(b2024,  "bldg_FLD_ZONE_2024")
+#   bpre_df   <- roll_zone(bprelim, "bldg_FLD_ZONE_2026")
+#
+#
+#   lomr2018_df <- blomr2018 |>
+#     as.data.frame() |>
+#     mutate(lomr_year = "2018") |>
+#     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
+#     distinct()
+#
+#   lomr2024_df <- blomr2024 |>
+#     as.data.frame() |>
+#     mutate(lomr_year = "2024") |>
+#     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
+#     distinct()
+#
+#   lomr_join <- lomr2018_df |>
+#     full_join(lomr2024_df,
+#       by = c("pin10", "CASE_NO", "EFF_DATE"),
+#       suffix = c("lomr2018", "lomr2024")
+#     )  |>
+#     rename(lomr_date = EFF_DATE)
+#
+#   # --- combine hits-only table ---
+#   out <- b2018_df |>
+#     dplyr::full_join(b2024_df, by = "pin10") |>
+#     dplyr::full_join(bpre_df,  by = "pin10") |>
+#     dplyr::full_join(lomr_join, by = "pin10")
+#
+#   out |> group_by(pin10) |> slice_head(n = 1) |> ungroup()
+# }
+
+make_sfha_indicator_buildings <- function(b2018, b2026, blomr2018, blomr2026) {
   # --- SFHA zone rollups (pin10-level) ---
   roll_zone <- function(x, out_name) {
     x |>
@@ -192,8 +365,7 @@ make_sfha_indicator_buildings <- function(b2018, b2024, bprelim, blomr2018, blom
   }
 
   b2018_df  <- roll_zone(b2018,  "bldg_FLD_ZONE_2018")
-  b2024_df  <- roll_zone(b2024,  "bldg_FLD_ZONE_2024")
-  bpre_df   <- roll_zone(bprelim, "bldg_FLD_ZONE_2026")
+  b2026_df  <- roll_zone(b2026,  "bldg_FLD_ZONE_2026")
 
 
   lomr2018_df <- blomr2018 |>
@@ -202,27 +374,71 @@ make_sfha_indicator_buildings <- function(b2018, b2024, bprelim, blomr2018, blom
     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
     distinct()
 
-  lomr2024_df <- blomr2024 |>
+  lomr2026_df <- blomr2026 |>
     as.data.frame() |>
-    mutate(lomr_year = "2024") |>
+    mutate(lomr_year = "2026") |>
     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
     distinct()
 
   lomr_join <- lomr2018_df |>
-    full_join(lomr2024_df,
+    full_join(lomr2026_df,
       by = c("pin10", "CASE_NO", "EFF_DATE"),
-      suffix = c("lomr2018", "lomr2024")
+      suffix = c("lomr2018", "lomr2026")
     )  |>
     rename(lomr_date = EFF_DATE)
 
   # --- combine hits-only table ---
   out <- b2018_df |>
-    dplyr::full_join(b2024_df, by = "pin10") |>
-    dplyr::full_join(bpre_df,  by = "pin10") |>
+    dplyr::full_join(b2026_df, by = "pin10") |>
     dplyr::full_join(lomr_join, by = "pin10")
 
   out |> group_by(pin10) |> slice_head(n = 1) |> ungroup()
 }
+#
+# make_sfha_indicator_buildings <- function(b2018, b2026, blomr2018, blomr2026) {
+#   # --- SFHA zone rollups (pin10-level) ---
+#   roll_zone <- function(x, out_name) {
+#     x |>
+#       sf::st_drop_geometry() |>
+#       dplyr::select(pin10, FLD_ZONE) |>
+#       dplyr::filter(!is.na(.data$pin10), !is.na(.data$FLD_ZONE)) |>
+#       dplyr::group_by(.data$pin10) |>
+#       dplyr::summarise(
+#         "{out_name}" := paste(sort(unique(.data$FLD_ZONE)), collapse = ";"),
+#         .groups = "drop"
+#       )
+#   }
+#
+#   b2018_df  <- roll_zone(b2018,  "bldg_FLD_ZONE_2018")
+#   b2024_df  <- roll_zone(b2026,  "bldg_FLD_ZONE_2026")
+#
+#
+#   lomr2018_df <- blomr2018 |>
+#     as.data.frame() |>
+#     mutate(lomr_year = "2018") |>
+#     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
+#     distinct()
+#
+#   lomr2026_df <- blomr2024 |>
+#     as.data.frame() |>
+#     mutate(lomr_year = "2026") |>
+#     select(pin10, lomr_year, EFF_DATE, CASE_NO) |>
+#     distinct()
+#
+#   lomr_join <- lomr2018_df |>
+#     full_join(lomr2024_df,
+#               by = c("pin10", "CASE_NO", "EFF_DATE"),
+#               suffix = c("lomr2018", "lomr2026")
+#     )  |>
+#     rename(lomr_date = EFF_DATE)
+#
+#   # --- combine hits-only table ---
+#   out <- b2018_df |>
+#     dplyr::full_join(b2026_df, by = "pin10") |>
+#     dplyr::full_join(lomr_join, by = "pin10")
+#
+#   out |> group_by(pin10) |> slice_head(n = 1) |> ungroup()
+# }
 
 
 read_nfhl_1in500 <- function(gdb_path, layer, border, sfha_tf_field = "SFHA_TF") {
