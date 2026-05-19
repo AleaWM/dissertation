5d287f19f8d58b21e54f8147e76c0846eb913ef5
AWM <awilbu2@uic.edu>
2026-05-19 11:46:22 -0500

    removes all references to zone_2024 and objects with "2024" in name.
    no longer necessary to use the 2024 effective map and the preliminary map after the most recent FIRM panels became effective in January 2026.
    
    No longer having to combine effective and preliminary maps simplified workflow a lot.

--- R/indicator_final_functions.R
+++ R/indicator_final_functions.R
@@ -5,40 +5,75 @@
 make_sfha_indicator_final <- function(parcels_with_firms,
                                       indicator_df,
                                       zone_2018,
-                                      zone_2024,
                                       zone_2026,
                                       lomr_date_col = "lomr_date",
                                       out_prefix = "sfha",
                                       join_by = "pin10") {
   z18 <- rlang::sym(zone_2018)
-  z24 <- rlang::sym(zone_2024)
   z26 <- rlang::sym(zone_2026)
   ld  <- rlang::sym(lomr_date_col)
 
   out18 <- paste0(out_prefix, "2018")
-  out24 <- paste0(out_prefix, "2024")
   out26 <- paste0(out_prefix, "2026")
 
   parcels_with_firms |>
     dplyr::left_join(indicator_df, by = join_by, relationship = "many-to-one") |>
     dplyr::mutate(
       !!out18 := dplyr::if_else(!is.na(!!z18), 1L, 0L),
-      !!out24 := dplyr::if_else(!is.na(!!z24), 1L, 0L),
-      !!out26 := dplyr::case_when(
-        in_prelim_panels == TRUE  & !is.na(!!z26) ~ 1L,
-        in_prelim_panels == TRUE  &  is.na(!!z26) ~ 0L,
-        in_prelim_panels == FALSE                 ~ .data[[out24]],
-        TRUE                                      ~ NA_integer_
-      ),
+      !!out26 := dplyr::if_else(!is.na(!!z26), 1L, 0L),
       lomr_date = as.Date(!!ld)
-    ) |>
-    dplyr::select(
-      pin10, longitude, latitude, start_year, end_year,
-      FIRM_PAN, VERSION_ID, PRE_DATE, EFF_DATE, in_prelim_panels,
-      dplyr::all_of(c(out18, out24, out26)),
-      lomr_date
-    )
+    ) # |>
+  # dplyr::select(
+  #   any_of(c(
+  #     "pin10", "longitude", "latitude", "start_year", "end_year",
+  #     "FIRM_PAN", "VERSION_ID", "PRE_DATE", "EFF_DATE", "in_prelim_panels",
+  #     "lomr_date", "CASE_NO",
+  #     dplyr::all_of(c(out18, out26))
+  #   )
+  #   )
+  # )
 }
+
+
+
+# # R/make_sfha_indicator_final.R
+# make_sfha_indicator_final <- function(parcels_with_firms,
+#                                       indicator_df,
+#                                       zone_2018,
+#                                       zone_2024,
+#                                       zone_2026,
+#                                       lomr_date_col = "lomr_date",
+#                                       out_prefix = "sfha",
+#                                       join_by = "pin10") {
+#   z18 <- rlang::sym(zone_2018)
+#   z24 <- rlang::sym(zone_2024)
+#   z26 <- rlang::sym(zone_2026)
+#   ld  <- rlang::sym(lomr_date_col)
+#
+#   out18 <- paste0(out_prefix, "2018")
+#   out24 <- paste0(out_prefix, "2024")
+#   out26 <- paste0(out_prefix, "2026")
+#
+#   parcels_with_firms |>
+#     dplyr::left_join(indicator_df, by = join_by, relationship = "many-to-one") |>
+#     dplyr::mutate(
+#       !!out18 := dplyr::if_else(!is.na(!!z18), 1L, 0L),
+#       !!out24 := dplyr::if_else(!is.na(!!z24), 1L, 0L),
+#       !!out26 := dplyr::case_when(
+#         in_prelim_panels == TRUE  & !is.na(!!z26) ~ 1L,
+#         in_prelim_panels == TRUE  &  is.na(!!z26) ~ 0L,
+#         in_prelim_panels == FALSE                 ~ .data[[out24]],
+#         TRUE                                      ~ NA_integer_
+#       ),
+#       lomr_date = as.Date(!!ld)
+#     ) |>
+#     dplyr::select(
+#       pin10, longitude, latitude, start_year, end_year,
+#       FIRM_PAN, VERSION_ID, PRE_DATE, EFF_DATE, in_prelim_panels,
+#       dplyr::all_of(c(out18, out24, out26)),
+#       lomr_date
+#     )
+# }
 
 #
 #
