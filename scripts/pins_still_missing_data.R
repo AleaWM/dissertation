9fa97c1bcf749acec0db1dad265d260d4d12e705
AWM <awilbu2@uic.edu>
2026-05-25 16:56:33 -0500

    notes on pins that were missing data and ultimately filtered out later. archive.

---
+++ scripts/pins_still_missing_data.R
@@ -0,0 +1,24 @@
+
+
+
+# pins missing data
+
+pin10_vec <- c(
+  "0427302008", # old parcel, northfield / Glenview area.  not FZ
+  "0427302009", # old parcel, northfield / Glenview area.  not FZ
+  "0633108005", # new buildings in Bartlett, next to the SFHA but not in it
+  "0633108006", # new buildings in Bartlett, next to the SFHA but not in it
+  "0633108007", # new buildings in Bartlett, next to the SFHA but not in it
+  "0633108008", # new buildings in Bartlett, next to the SFHA but not in it
+  "0633108009",  # land around the new buildings
+  "0633200015", # more new buildings in bartlett: Wood Lily Ct.
+  "0633205020", # bartlett, Bluebell LN
+  "1705320076", # no building here?
+  "1710130027", # by chicago riverbut not floodzone
+  "1710400054", # by chicago riverbut not floodzone, no buildinghere?
+  "2226307004", # new homesin Lemont. The onesthat use to be a golf course! not FZ
+  "2226307005", # new homesin Lemont. The onesthat use to be a golf course! not FZ
+  "2433302053", # nobuildinghere
+  "2730212019", # in A LOMR since 2018, Clover Drive, Orland Park. New Buildings. By floodzonebut not in floodzone
+  "2804400093"   # midlothian turnpike. by floodzone but not in floodzone
+)
